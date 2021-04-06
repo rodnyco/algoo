10042d5ffe1bc358c57f5d54e977c2dbc6e4b2d6
Владимир Раду <rodny_co@list.ru>
2021-04-07 00:57:38 +0300

    Final stack array

--- src/DataStructurs/Stack.java
+++ src/DataStructurs/Stack.java
@@ -1,21 +1,27 @@
 package DataStructurs;
 
 public class Stack {
-    private int[] elements;
-    private int top;
-
     public static void main(String[] args) throws Exception {
         Stack myStack = new Stack(4);
         myStack.push(1);
         System.out.println(myStack.getTop());
         myStack.push(10);
         myStack.push(20);
+        myStack.push(20);
+        myStack.push(20);
+        myStack.push(20);
+        myStack.push(20);
         myStack.pop();
         System.out.println(myStack.getTop());
     }
 
+    private final int size;
+    private final int[] elements;
+    private int top;
+
     public Stack(int length) {
-        elements = new int[length];
+        size = length;
+        elements = new int[size];
         top = -1;
     }
 
@@ -23,18 +29,19 @@ public class Stack {
         return top == -1;
     }
 
-    public void push(int el) {
+    public void push(int el) throws Exception {
+        if(top+1 == size) throw new Exception("Stack Overflow");
         top++;
         elements[top] = el;
     }
 
     public void pop() throws Exception {
-        if(isEmpty()) throw new Exception("Stack is empty :(");
+        if(isEmpty()) throw new Exception("Stack is empty");
         top--;
     }
 
     public int getTop() throws Exception {
-        if(isEmpty()) throw new Exception("Stack is empty :(");
+        if(isEmpty()) throw new Exception("Stack is empty");
 
         return elements[top];
     }
